ceff79281b463e028c47b5ffb61dbc2a6da1a38d
Oluwatobi Odekunle <ooluwatobialao@gmail.com>
2022-05-09 20:00:41 +0100

    initial commit:

---
+++ app/Http/Controllers/PhotoController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PhotoUploadStoreRequest;
+use App\Models\Photo;
+use Illuminate\Http\Request;
+
+class PhotoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('upload');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+    
+        $photo = $request->file('file');
+
+        $originalName = explode('.', $photo->getClientOriginalName())[0];
+        
+        $photoName = $originalName . '.' . time() . '.' . $photo->extension();
+
+        $photo->move(storage_path('images'), $photoName);
+
+        return response()->json(['success' => true, 'data' => $photoName]);
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Photo  $photo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Photo $photo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Photo  $photo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Photo $photo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Photo  $photo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Photo $photo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Photo  $photo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Photo $photo)
+    {
+        //
+    }
+}
